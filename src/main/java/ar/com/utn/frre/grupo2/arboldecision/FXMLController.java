03967a0ddedb0835b3ab103d2348748af724579d
Ulises Anich <anich.ulises@habitar.com.ar>
2019-05-13 20:41:43 -0300

    Mejoras visuales en notificaciones

--- src/main/java/ar/com/utn/frre/grupo2/arboldecision/FXMLController.java
+++ src/main/java/ar/com/utn/frre/grupo2/arboldecision/FXMLController.java
@@ -5,6 +5,9 @@ import ar.com.utn.frre.grupo2.arboldecision.dto.ElementoDTO;
 import ar.com.utn.frre.grupo2.arboldecision.dto.NodoDTO;
 import ar.com.utn.frre.grupo2.arboldecision.dto.RangosDTO;
 import ar.com.utn.frre.grupo2.arboldecision.service.ArbolDecisionService;
+import eu.hansolo.enzo.notification.Notification;
+import eu.hansolo.enzo.notification.NotificationBuilder;
+import eu.hansolo.enzo.notification.NotifierBuilder;
 import java.io.File;
 import java.math.BigDecimal;
 import java.net.URL;
@@ -13,10 +16,12 @@ import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import org.beanio.InvalidRecordException;
 
 public class FXMLController implements Initializable {
@@ -24,13 +29,13 @@ public class FXMLController implements Initializable {
     private Stage stage;
 
     private final ArbolDecisionService arbolesService = new ArbolDecisionService();
+    private Notification.Notifier notifier;
 
     private final ElementosDAO elementosDAO = new ElementosDAO();
     private final List<ElementoDTO> elementos = new ArrayList<>();
     private NodoDTO nodoRaiz = null;
 
     private final Alert confirmAlert = new Alert(Alert.AlertType.CONFIRMATION);
-    private final Alert infoAlert = new Alert(Alert.AlertType.INFORMATION);
 
     private final ResourceBundle mensajes = ResourceBundle.getBundle("strings/mensajes");
 
@@ -41,7 +46,6 @@ public class FXMLController implements Initializable {
     private CanvasGrafico canvas;
     @FXML
     private CanvasArbol canvasArbol;
-
 
     @FXML
     private void importarElementos() {
@@ -54,17 +58,18 @@ public class FXMLController implements Initializable {
             try {
                 elementos.addAll(elementosDAO.obtenerDatosCSV(archivo));
             } catch (InvalidRecordException e) {
-                informar(mensajes.getString("importacion_elementos_error_titulo"),
-                        mensajes.getString("importacion_elementos_error_header"),
-                        mensajes.getString("importacion_elementos_error_mensaje"));
+                notifier.notify(NotificationBuilder.create()
+                        .title(mensajes.getString("importacion_elementos_error_titulo"))
+                        .message(mensajes.getString("importacion_elementos_error_mensaje"))
+                        .image(Notification.ERROR_ICON).build());
             }
 
-
             if (!elementos.isEmpty()) {
+                notifier.notify(NotificationBuilder.create()
+                        .title(mensajes.getString("importacion_elementos_titulo"))
+                        .message(mensajes.getString("importacion_elementos_mensaje"))
+                        .image(Notification.INFO_ICON).build());
 
-                informar(mensajes.getString("importacion_elementos_titulo"),
-                        mensajes.getString("importacion_elementos_header"),
-                        mensajes.getString("importacion_elementos_mensaje"));
             }
         }
 
@@ -96,19 +101,9 @@ public class FXMLController implements Initializable {
         System.out.println("Proceso finalizado!");
     }
 
-
-
-    private void informar(String titulo, String encabezado, String pregunta) {
-        infoAlert.setTitle(titulo);
-        infoAlert.setHeaderText(encabezado);
-        infoAlert.setContentText(pregunta);
-        infoAlert.showAndWait();
-    }
-
     @Override
     public void initialize(URL url, ResourceBundle rb) {
     }
-
 
     public Stage getStage() {
         return stage;
@@ -116,6 +111,12 @@ public class FXMLController implements Initializable {
 
     public void setStage(Stage stage) {
         this.stage = stage;
+        notifier = NotifierBuilder.create()
+                .popupLocation(Pos.TOP_RIGHT)
+                .popupLifeTime(Duration.millis(10000))
+                //.styleSheet(getClass().getResource("mynotification.css").toExternalForm())
+                .build();
+        stage.setOnCloseRequest(observable -> notifier.stop());
     }
 
     private void recargarTabla() {
